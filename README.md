4aced0a78050d26632a3885c9514397c45f3ac76
Navee Sratthatad <n4veee@gmail.com>
2025-06-03 22:20:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rust Learning Projects
+
+This repo contains daily Rust practice projects.
+
+# To Create New Days
+cargo new day01_hello --vcs none
+
+## Contents
+
+- [Day 01 - Hello Rust](day01_hello/)
+- [Day 02 - TODO]
---
+++ day01_hello/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+fn main() {
+    println!("Hello...");
+    println!("Who are you ?");
+
+    let mut input: String = String::new();
+
+    let stdin = io::stdin();
+
+    stdin.read_line(&mut input)
+    .expect("Read fails");
+
+    println!("Hello {}", input)
+}
